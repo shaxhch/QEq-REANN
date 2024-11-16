fc8620d815a14719224fdfd7cf4024beacc9740c
shaxh <113909459+shaxhch@users.noreply.github.com>
2024-11-16 12:12:18 +0800

    Delete core directory

--- core/Qeq-REANN Fortran interface/reann-testch/build/clean-cmake.sh
+++
@@ -1,2 +0,0 @@
-rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake trash/
-rm -rf ./bin/ ./lib/ ./modules/ Makefile
--- core/Qeq-REANN Fortran interface/reann-testen/build/build.sh
+++
@@ -1,6 +0,0 @@
-cmake \
- -D BUILD_CUDA=OFF \
- -D CMAKE_Fortran_COMPILER=`which gfortran` \
- -D CMAKE_CXX_COMPILER=`which g++` \
- -D CMAKE_C_COMPILER=`which gcc` \
- ../cmake
